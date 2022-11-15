640dc560dbc6850b9f23c35ab3b3cfb117fa45d2
wenleyan <wenleyan@umich.edu>
2022-11-14 19:12:45 -0500

    cleaned code, still work

--- main.py
+++ main.py
@@ -23,12 +23,8 @@ data_dir = os.path.join(date_dir, f'scene-{scene_no}')
 img_extension = 'jpeg'
 checkpoint_file = f'{date_dir}\scene-{scene_no}-2Dkps.pkl'
 kp_nos = 8
-portrait_flag = True
-oct31Test = True
-if portrait_flag:
-    cam_yml = r'H:\phone_Lidar\data\prelim\oct11\Hongxiao_portrait.yml'
-else:
-    cam_yml = r'H:\phone_Lidar\data\prelim\oct11\Hongxiao.yml'
+cam_yml = r'H:\phone_Lidar\data\prelim\oct11\Hongxiao_portrait.yml'
+
 ####################################### CHANGE HERE BEFORE RUN #######################################
 
 ### Read 2D keypoint annotation: pkl
@@ -40,9 +36,6 @@ with open(checkpoint_file, 'rb') as f:
 
 ### Read phone data: yml
 camera_matrix, dist_coeffs = load_coefficients(cam_yml)
-cam_intrinsic_4x4M = np.eye(4)
-cam_intrinsic_4x4M[:3, :3] = camera_matrix
-
 
 cameraPositions = []
 lineP_3ds = []
@@ -80,21 +73,9 @@ for index, frame in annotation.iterrows():
         cameraEulerAngles = data['cameraEulerAngles'] # ios-ARkit XYZ Roll-Pitch-Yaw
         camera_rot_3x3M = rotation_matrix(cameraEulerAngles[0], cameraEulerAngles[1], cameraEulerAngles[2])
         cameraTransform = np.array(data['cameraTransform'][0]).T
-        if oct31Test:
-            cameraIntrinsicsInversed = np.array(data['cameraIntrinsicsInversed']).reshape((3,3))
-            localToWorld = np.array(data['localToWorld']).reshape((4,4))
-            depth_cam_intrinsic_3x3M = np.linalg.pinv(cameraIntrinsicsInversed) # not sure
-
-        if True:
-            # if ios case is set to gravity (0)
-            camera_rot_4x4M = np.eye(4)
-            camera_rot_4x4M[:3, :3] = rotation_matrix(-np.pi, 0, 0) @ camera_rot_3x3M
-            camera_transform_4x4M = np.eye(4)
-            camera_transform_4x4M[:3, 3] = cameraTransform[:3, 3]
-            cam_extrinsic_4x4M = camera_transform_4x4M @ camera_rot_4x4M
-
-
-            cameraTransform[:-1,:-1] = (camera_rot_3x3M.T) @ (cameraTransform[:-1,:-1]) #
+        localToWorld = np.array(data['localToWorld']).reshape((4,4))
+        # depthCameraIntrinsicsInversed = np.array(data['cameraIntrinsicsInversed']).reshape((3,3))
+        # depth_cam_intrinsic_3x3M = np.linalg.pinv(depthCameraIntrinsicsInversed)
 
         cameraPosition = cameraTransform[:-1,-1]
         cameraPositions.append(cameraPosition)
@@ -133,44 +114,25 @@ for index, frame in annotation.iterrows():
     #                          [5760/2, 4320/2],[5760/2, 4320/2],[5760/2, 4320/2],[5760/2, 4320/2],])
 
     kp_no = frame.img_kp.shape[0]
-    #     add 1 & 1/z to the last column of img_kp
-    #     https://medium.com/yodayoda/from-depth-map-to-point-cloud-7473721d3f
-    # https://developer.apple.com/documentation/arkit/arconfiguration/worldalignment/gravity
 
-    if portrait_flag:
-        #  this is portrait
-        rot_kp = np.array([[0,1],[1,0]]).dot(frame.img_kp.T).T+np.array([4320,0])
-        dist_kp = rot_kp.astype(np.float32).reshape((kp_no,1,  2))
-        undist_kp = cv2.undistortPoints(dist_kp, camera_matrix, dist_coeffs, P=camera_matrix).reshape((kp_no, 2))
+    #  this is portrait
+    rot_kp = np.array([[0,1],[1,0]]).dot(frame.img_kp.T).T+np.array([4320,0])
+    dist_kp = rot_kp.astype(np.float32).reshape((kp_no,1,  2))
+    undist_kp = cv2.undistortPoints(dist_kp, camera_matrix, dist_coeffs, P=camera_matrix).reshape((kp_no, 2))
 
 
-        img_kp = np.hstack((rot_kp, np.ones((kp_no, 1)), 1/pesdoDepth*np.ones((kp_no, 1))))
-    else:
-        # this is landscape (annotation is landscape)
-        dist_kp = frame.img_kp.astype(np.float32).reshape((kp_no,1,  2))
-        undist_kp = cv2.undistortPoints(dist_kp, camera_matrix, dist_coeffs, P=camera_matrix).reshape((kp_no, 2))
-        img_kp = np.hstack((undist_kp, np.ones((kp_no, 1)), 1/pesdoDepth*np.ones((kp_no, 1))))
-    #     why 1/pesdoDepth: https://medium.com/yodayoda/from-depth-map-to-point-cloud-7473721d3f
-    if not oct31Test:
-        # lineP_3d = np.dot(np.linalg.pinv(1 / pesdoDepth * np.dot(cam_intrinsic_4x4M, cam_extrinsic_4x4M)), img_kp.T).T
-        lineP_3d = (
-            np.linalg.pinv(1 / pesdoDepth *
-                           (cam_intrinsic_4x4M @ cam_extrinsic_4x4M)) @ img_kp.T
+    localPoint = (np.linalg.pinv(camera_matrix) @
+                  np.hstack(
+                      (undist_kp, np.ones((kp_no, 1)))
+                            ).T
+                  ).T*pesdoDepth
+    lineP_3d = (localToWorld.T @
+        np.hstack(
+            (
+                localPoint, np.ones((kp_no, 1))
+            )
         ).T
-
-    else:
-        localPoint = (np.linalg.pinv(camera_matrix) @
-                      np.hstack(
-                          (undist_kp, np.ones((kp_no, 1)))
-                                ).T
-                      ).T*pesdoDepth
-        lineP_3d = (localToWorld.T @
-            np.hstack(
-                (
-                    localPoint, np.ones((kp_no, 1))
-                )
-            ).T
-        ).T
+    ).T
 
 
     lineP_3d = np.array([np.divide(lineP_3d[:,0],lineP_3d[:,3].T),np.divide(lineP_3d[:,1],lineP_3d[:,3].T),np.divide(lineP_3d[:,2],lineP_3d[:,3].T)]).T
